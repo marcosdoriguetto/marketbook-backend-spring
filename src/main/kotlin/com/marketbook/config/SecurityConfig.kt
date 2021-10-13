c3cf109da3b32b37fb69cdda9d4f671b3b6ce240
Marcos <marcosdoriguetto@gmail.com>
2021-10-13 11:49:46 -0300

    Criação da gwreração do token(JWT)

--- src/main/kotlin/com/marketbook/config/SecurityConfig.kt
+++ src/main/kotlin/com/marketbook/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.marketbook.config
 
 import com.marketbook.repository.CustomerRepository
 import com.marketbook.security.AuthenticationFilter
+import com.marketbook.security.JwtUtil
 import com.marketbook.service.UserDetailsCustomService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,7 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 @EnableWebSecurity
 class SecurityConfig(
     private val customerRepository: CustomerRepository,
-    private val userDetails: UserDetailsCustomService
+    private val userDetails: UserDetailsCustomService,
+    private val jwtUtil: JwtUtil
 ) : WebSecurityConfigurerAdapter() {
 
     private val PUBLIC_MATCHERS = arrayOf<String>()
@@ -36,7 +38,7 @@ class SecurityConfig(
             .antMatchers(*PUBLIC_MATCHERS).permitAll()
             .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
             .anyRequest().authenticated()
-        http.addFilter(AuthenticationFilter(authenticationManager(), customerRepository))
+        http.addFilter(AuthenticationFilter(authenticationManager(), customerRepository, jwtUtil))
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
